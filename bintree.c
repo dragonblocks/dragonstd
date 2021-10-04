5cb6a0956cd87981071f7e3dd5669f9698ac6682
Elias Fleckenstein <eliasfleckenstein@web.de>
2021-10-04 11:33:39 +0200

    Remove implementation.c and fix bintree.c

--- bintree.c
+++ bintree.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "bintree.h"
 
 typedef struct
--- implementation.c
+++
@@ -1,7 +0,0 @@
-// include *.c files
-
-#include "array.c"
-#include "bintree.c"
-#include "list.c"
-#include "number.c"
-#include "queue.c"
